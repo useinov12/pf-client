b5a19aaec13840ea8de36edbeee3808ca262b5de
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-14 10:36:31 -0400

    fix: line controller fix

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -2,10 +2,10 @@ import React, { useRef, useEffect, useState } from 'react';
 import type { ChartData, ChartArea } from 'chart.js';
 import {
   Chart as ChartJS,
-  registerables,
   CategoryScale,
   LinearScale,
   PointElement,
+  LineController,
   LineElement,
   Tooltip,
   Legend,
@@ -14,6 +14,7 @@ import {
 ChartJS.register(
   CategoryScale,
   LinearScale,
+  LineController,
   PointElement,
   LineElement,
   Tooltip,
