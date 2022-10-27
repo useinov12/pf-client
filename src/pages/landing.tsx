704472062165e7c7ec7613e2d1d5f3b84642be88
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-27 07:51:10 -0400

    refactor: lannding page init

---
+++ src/pages/landing.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import clsx from 'clsx';
+import { Toaster } from 'react-hot-toast';
+import { ThemeContext } from '@/context/ThemeProvider';
+
+import ArrowLink from '@/components/links/ArrowLink';
+import ButtonLink from '@/components/links/ButtonLink';
+import UnstyledLink from '@/components/links/UnstyledLink';
+import Accent from '@/components/Accent';
+import Header from '@/components/layout/Header';
+import Layout from '@/components/layout/Layout';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <main className={clsx('max-w-screen-xl', 'mx-auto')}>
+        
+      </main>
+    </Layout>
+  );
+}
