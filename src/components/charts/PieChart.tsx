8d5e2ec15b251e1ad1b8d391daf1dc59fb7f7ae2
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-03 08:42:30 -0400

    refactor: external data prop

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,90 +1,75 @@
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import { Chart } from 'react-chartjs-2';
 
-import type { ChartData, ChartArea, ScriptableContext } from 'chart.js';
+import type { ChartData, ScriptableContext } from 'chart.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart: React.FC<{
   radius: string;
   isFakeData?: boolean;
-  data?: number[];
+  externalData?: number[];
   labels?: string[];
-}> = ({ radius, isFakeData, data, labels }) => {
+  delay?: number;
+}> = ({ radius, isFakeData, externalData, labels, delay }) => {
+  const chartRef = React.useRef<ChartJS>(null);
+  const [chartData, setChartData] = React.useState<ChartData<'pie'>>({
+    datasets: [],
+  });
 
-  const [chartData, setChartData] = React.useState<any>(fakeDataset)
+  React.useEffect(() => {
+    const chart = chartRef.current;
 
-  React.useEffect(()=>{
-    if(data){
-      setChartData(
-        {
-          labels: labels,
-          datasets: [
-            {
-              data: data,
-              backgroundColor: [
-                'rgba(255, 99, 132, .5)',
-                'rgba(54, 162, 235, .5)',
-                'rgba(255, 206, 86, .5)',
-                'rgba(12, 206, 86, .5)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(12, 206, 86, 1)',
-              ],
-              borderWidth: 3,
-            },
-          ],
-        }
-      )
+    if (!chart) {
+      return;
     }
-    else  setChartData(fakeDataset)
 
-  },[data])
+    const data = {
+      labels: labels ? labels : ['Data#1', 'Data#2', 'Data#3', 'Data#4'],
+      datasets: [
+        {
+          data: externalData ? externalData : [7, 19, 3, 5],
+          backgroundColor: [
+            'rgba(255, 99, 132, .5)',
+            'rgba(54, 162, 235, .5)',
+            'rgba(255, 206, 86, .5)',
+            'rgba(12, 206, 86, .5)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(12, 206, 86, 1)',
+          ],
+          borderWidth: 3,
+        },
+      ],
+    };
 
-  return (
-    <Pie
-      data={chartData}
-      options={{
-        responsive: true,
-        maintainAspectRatio: true,
-        radius: radius,
-        plugins:{
-          legend:{
-            display:false
-          }
-        }
-      }}
-    />
-  );
+    if (delay) {
+      const timer = setTimeout(() => {
+        setChartData(data);
+      }, delay);
+      return () => clearTimeout(timer);
+    } else setChartData(data);
+  }, [externalData]);
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    radius: radius,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  return <Chart type='pie' ref={chartRef} data={chartData} options={options} />;
 };
 
 export default PieChart;
-
-export const fakeDataset = {
-  labels: ['Data#1', 'Data#2', 'Data#3', 'Data#4'],
-  datasets: [
-    {
-      data: [7, 19, 3, 5],
-      backgroundColor: [
-        'rgba(70, 70, 70, .2)',
-        'rgba(70, 70, 70, .2)',
-        'rgba(70, 70, 70, .2)',
-        'rgba(70, 70, 70, .2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(12, 206, 86, 1)',
-      ],
-      borderWidth: 4,
-    },
-  ],
-};
 
 function createGradient({ context }: { context: ScriptableContext<'pie'> }) {
   const ctx = context.chart.ctx;
