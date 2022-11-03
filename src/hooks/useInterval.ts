e83693572c91ae0921ee90c7729114fc75ee48ab
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-03 18:00:51 -0400

    feat: useinterval hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface IUseInterval {
+  (callback: () => void, interval: number| null): void;
+}
+
+const useInterval:IUseInterval = (callback, interval) => {
+    const savedCallback = React.useRef<()=>void>(()=>{});
+  
+    React.useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+  
+    React.useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if(interval){
+        let id = setInterval(tick, interval);
+        return () => clearInterval(id);
+      }
+    }, [interval]);
+  };
+
+export default useInterval
