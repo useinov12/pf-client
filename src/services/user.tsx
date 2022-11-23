eb48494ee95f5c7a31379468452b293d403dd4c2
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-22 19:08:24 -0500

    docs: register and login functions

--- src/services/user.tsx
+++ src/services/user.tsx
@@ -1,42 +1,46 @@
 import { Dispatch, createContext, useReducer, useContext } from 'react';
 import Cookies from 'js-cookie';
 import toast from 'react-hot-toast';
+import { useQuery } from '@tanstack/react-query';
 
 import {
   Credentials,
+  LoginCredentials,
   createNewUser as apiCreateNewUser,
   loginUser as apiLoginUser,
-  LoginCredentials,
+  getMe as apiGetMe,
 } from './api';
 
 /**
  * *Sends Post request to Server*\
  * Notifies user about results of the request with `toast`
- * @description `register` is an `Action` - API call that 
+ * @description `register` is an `Action` - API call that
  * does not have an effect on cached entity data
+ * @param {Credentials} Credentials
  * @returns {number} `Status Code`
  * */
-export async function register(signUpCred: Credentials){
+export async function register(signUpCred: Credentials) {
   try {
     const { status } = await apiCreateNewUser(signUpCred);
     toast.success('You are successfully registered!');
-    return { status:status };
+    return { status: status };
   } catch (error: any) {
     if (error.response.status === 404) {
       toast.error(error.response.data.detail.message);
     } else {
       toast.error('An unexpected error occurred :(  Try again.');
     }
-    return {status:error.response.status}
+    return { status: error.response.status };
   }
 }
 
 /**
- * *Authentificate user*\
+ * **Authentificate user**\
  * Sets JWT as a cookie\
- * Notifies user about results of the request with `toast`
- * @description `login` is an `Action` - API call that 
+ * Notifies user about results of the request with `toast`\
+ * **@description** `login` is an `Action` - API call that
  * does not have an effect on cached entity data
+ * @param {LoginCredentials} LoginCredentials
  */
 export async function login(loginCred: LoginCredentials) {
   try {
@@ -50,6 +54,21 @@ export async function login(loginCred: LoginCredentials) {
       toast.error('An unexpected error occurred :(  Try again.');
     }
   }
+}
+
+/**
+ * **Authorize current User**\
+ * A custom hook that wraps a react-query hook\
+ * **@description**
+ * `useUser` is a `Query Hook` -  API call that
+ * api call that influences a cached entity
+ *  */
+export function useUser(){
+    return useQuery(['user'], apiGetMe, {
+      retry: 0,
+      // should be refetched in the background every x hours
+      // staleTime: 1000 * 60 * 60 * x,
+    });
 }
 
 interface User {
