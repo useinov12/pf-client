7693f455e381c1f311388b1f4ce014fcf0a8625f
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-30 13:24:52 -0500

    reafctor: cabinet component isolated

--- src/pages/cabinet.tsx
+++ src/pages/cabinet.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import clsx from 'clsx';
-import Image from 'next/image';
 import { ThemeContext } from '@/context/ThemeProvider';
 import Button from '@/components/buttons/Button';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import LaunchLink from '@/components/plaid/LaunchLink';
 import { PlaidLinkProvider, usePlaid } from '@/services/plaid';
 import ThemeButton from '@/components/homepage/ThemeButton';
+import { useUser } from '@/services/user';
+import Logo from '@/components/Logo';
 
 export default function CabinetPage() {
   return (
@@ -18,14 +17,19 @@ export default function CabinetPage() {
 }
 CabinetPage.requireAuth = true;
 
-
-
-
-
 const Cabinet = () => {
   const { mode } = React.useContext(ThemeContext);
-  // const { handleLogout } = React.useContext(UserContext);
 
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 100);
+    return () => clearTimeout(timer);
+  }, []);
+
+  // handleLogout
   // const { linkToken, generateLinkToken } = usePlaid();
 
   return (
@@ -33,7 +37,6 @@ const Cabinet = () => {
       className={clsx(
         'h-screen w-screen',
         'overflow-hidden',
-        // mode === 'dark' ? 'bg-dark text-gray-100' : 'bg-gray-200 text-gray-800',
         mode === 'dark' ? 'text-gray-100' : 'text-gray-800',
         mode === 'dark'
           ? 'bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black '
@@ -43,13 +46,17 @@ const Cabinet = () => {
       <div
         className={clsx(
           'mx-auto px-3 sm:max-w-screen-sm',
-          'md:max-w-screen-md ',
+          'md:max-w-screen-lg ',
           'lg:max-w-screen-xl',
-          'h-full w-full'
+          'h-full w-full',
+          isLoaded && 'fade-in-start'
         )}
       >
         <Navbar />
-        <main className='flex h-5/6 w-full flex-col gap-3 md:flex-row'>
+        <main
+          className='flex h-5/6 w-full flex-col gap-3 md:flex-row'
+          data-fade='2'
+        >
           <UserSection />
           <SettingsSection />
         </main>
@@ -59,107 +66,107 @@ const Cabinet = () => {
 };
 
 const Navbar = () => {
-  const router = useRouter();
-  // const { handleLogout } = React.useContext(UserContext);
-
   return (
-    <header className='flex items-center justify-between py-3'>
-      <Link href='/'>
-        <Image
-          src={'/images/logo.png'}
-          width={70}
-          height={64}
-          className='cursor-pointer'
-        />
-      </Link>
-
+    <nav
+      className='flex items-center justify-between py-3'
+      data-fade='1'
+    >
+      <Logo />
       <div className='flex items-center gap-2'>
-        <Button
-          className='text-md py-1'
-          variant='theme-dependent'
-        >
+        <Button className='text-md py-1' variant='theme-dependent'>
           Logout
         </Button>
-        <ThemeButton/>
+        <ThemeButton />
       </div>
-    </header>
+    </nav>
   );
 };
 
 const UserSection = () => {
   const { mode } = React.useContext(ThemeContext);
-  // const { user, handleLogout } = React.useContext(UserContext);
-  // const {data, isLoading, isSuccess} = useQueryUser()
+  const { user } = useUser();
 
   return (
-    <section
-      className={clsx(
-        'rounded-md border  py-7 md:h-full md:w-1/4 lg:px-7',
-        'flex items-center justify-around  md:flex-col md:justify-start',
-        mode === 'light' ? 'border-gray-100/50' : 'border-gray-300/20',
-        mode === 'light' ? 'bg-dark/10' : 'bg-gray-300/10',
-        'drop-shadow'
-      )}
-    >
-      <div className='flex flex-col items-center justify-center'>
-        <figure className='mb-3 h-16 w-16 rounded-full ring-4 ring-primary-300 ' />
-        <h4 className='mb-5'>
-          {/* {user && `${user.firstName} ${user.lastName}`} */}
-        </h4>
-      </div>
-      <div>
-        <Button
-          className={clsx(
-            'my-1 flex items-center justify-center  py-1 text-sm',
-            'w-36'
-          )}
-        >
-          Change Name
-        </Button>
-        <Button
-          variant='red'
-          className={clsx(
-            'my-1 flex items-center justify-center  py-1 text-sm',
-            'w-36'
-          )}
-        >
-          Delete account
-        </Button>
-      </div>
-    </section>
+    <MenuSection className='md:w-2/6 lg:w-1/5 '>
+      <header
+        className={clsx(
+          'w-full',
+          'flex items-center justify-between px-7 py-4',
+          mode === 'light' ? 'bg-gray-300/70' : 'bg-gray-900/70'
+        )}
+      >
+        <h4 className=''>Account</h4>
+      </header>
+
+      <section
+        className={clsx(
+          'px-7',
+          'flex items-center justify-around',
+          'md:flex-col md:justify-start md:items-start'
+        )}
+      >
+        <div className='my-3 flex flex-col items-start justify-start'>
+          <h1
+            className={clsx(
+              'my-3 flex h-12 w-14',
+              'items-center justify-center rounded-xl',
+              'text-5xl ring-4 ring-primary-500',
+              'drop-shadow'
+            )}
+          >
+            {user && `${user.firstName[0]}`}
+          </h1>
+          <h4 className=''>{user && `${user.firstName} ${user.lastName}`}</h4>
+          <p className='mb-5'>{user && `${user.username}`}</p>
+        </div>
+        <div>
+          <Button
+            className={clsx(
+              'flex items-center justify-center',
+              'my-1 w-36 py-1',
+              'text-sm'
+            )}
+          >
+            Change Name
+          </Button>
+          <Button
+            variant='red'
+            className={clsx(
+              'flex items-center justify-center',
+              'my-1 w-36 py-1 ',
+              'text-sm'
+            )}
+          >
+            Delete account
+          </Button>
+        </div>
+      </section>
+    </MenuSection>
   );
 };
 
 const SettingsSection = () => {
   const { mode, setMode } = React.useContext(ThemeContext);
-  // const { handleLogout } = React.useContext(UserContext);
 
   const { linkToken, generateLinkToken } = usePlaid();
   return (
-    <section
-      className={clsx(
-        'h-full overflow-hidden rounded border md:w-4/5',
-        mode === 'light' ? 'border-gray-100/50' : 'border-gray-300/20',
-        mode === 'light' ? 'bg-dark/20' : 'bg-gray-300/10',
-        'relative'
-      )}
-    >
-      <div
+    <MenuSection className='relative md:w-4/6 lg:w-4/5 h-full'>
+      <header
         className={clsx(
           'flex items-center justify-between px-7 py-4',
           mode === 'light' ? 'bg-gray-300/70' : 'bg-gray-900/70'
         )}
       >
-        <h4 className=''>Connected Bank</h4>
+        <h4 className=''>Connected Banks</h4>
         <Button
           variant='green'
-          className='w-32 whitespace-nowrap px-8 py-1 text-sm'
+          className='w-24 whitespace-nowrap py-1 text-sm'
           onClick={generateLinkToken}
         >
           Add bank
         </Button>
         {linkToken && <LaunchLink token={linkToken} />}
-      </div>
+      </header>
 
       <ul className='scroll-y h-full overflow-y-scroll'>
         {['A', 'B', 'C', 'D'].map((bank, i) => (
@@ -171,16 +178,49 @@ const SettingsSection = () => {
             )}
           >
             <h5 className='font-semibold'> Bank #{bank}</h5>
-            <Button
-              isDarkBg={mode === 'dark' && true}
-              variant='red-outline'
-              className='w-32 px-8 py-1 text-sm'
-            >
-              Remove
-            </Button>
+            <div className='flex gap-2'>
+              <Button
+                isDarkBg={mode === 'dark' && true}
+                variant='light'
+                className='flex w-24 items-center justify-center whitespace-nowrap py-1 text-sm'
+              >
+                Configure
+              </Button>
+              <Button
+                isDarkBg={mode === 'dark' && true}
+                variant='red'
+                className='flex w-24 items-center justify-center py-1 text-sm'
+              >
+                Remove
+              </Button>
+            </div>
           </li>
         ))}
       </ul>
-    </section>
+    </MenuSection>
+  );
+};
+
+const MenuSection = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: JSX.Element | JSX.Element[];
+}) => {
+  const { mode } = React.useContext(ThemeContext);
+
+  return (
+    <div
+      className={clsx(
+        'rounded-md border md:h-full',
+        mode === 'light' ? 'border-gray-100/50' : 'border-gray-300/20',
+        mode === 'light' ? 'bg-dark/10' : 'bg-gray-300/10',
+        'overflow-hidden drop-shadow',
+        className
+      )}
+    >
+      {children}
+    </div>
   );
 };
