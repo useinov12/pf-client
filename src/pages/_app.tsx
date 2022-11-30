f0db2112166d6a5934bda7cf9dda99ce70d0721a
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-30 09:40:06 -0500

    refactor: providers moved down by their trees

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
+import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PlaidTokenProvider from '@/context/PlaidTokenProvider';
-import { UserProvider } from '@/services/user';
+import {  UserProvider, AuthGuard } from '@/services/user';
 import ThemeProvider from '@/context/ThemeProvider';
-import LoginFormProvider from '@/context/LoginFormProvider';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+export type NextApplicationPage<P = any, IP = P> = NextPage<P, IP> & {
+  requireAuth?: boolean;
+};
 
 import '@/styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+function MyApp(props: AppProps) {
+  const {
+    Component,
+    pageProps,
+  }: { Component: NextApplicationPage; pageProps: any } = props as AppProps;
 
+  const queryClient = new QueryClient() 
   return (
     <>
       <Toaster />
-      <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+      {/* <QueryClientProvider client={queryClient}> */}
         <UserProvider>
-          <PlaidTokenProvider>
-            <ThemeProvider>
-              <LoginFormProvider>
-                <Component {...pageProps} />
-              </LoginFormProvider>
-            </ThemeProvider>
-          </PlaidTokenProvider>
+          {/* if requireAuth property is present - protect the page */}
+          {Component.requireAuth ? (
+            <AuthGuard>
+              <Component {...pageProps} />
+            </AuthGuard>
+          ) : (
+            // public page
+            <Component {...pageProps} />
+          )}
         </UserProvider>
-      </QueryClientProvider>
+        {/* </QueryClientProvider> */}
+      </ThemeProvider>
     </>
   );
 }
