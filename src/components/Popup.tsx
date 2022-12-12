6bb3592c410c591b3f6d70e2799e28dc6019b77c
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-12 15:55:04 -0500

    feat: popup component

---
+++ src/components/Popup.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
+import Button from './buttons/Button';
+import Card from './page/landing/cards/Card';
+import { AiOutlineClose } from 'react-icons/ai';
+
+export const Popup = ({
+  children,
+  open,
+  handleOpen,
+  withCloseBtn,
+  withOkBtn,
+}: {
+  children: JSX.Element;
+  open: boolean;
+  handleOpen: () => void;
+  withCloseBtn?: boolean;
+  withOkBtn?: boolean;
+}) => {
+  const [openLocalState, setOpenLocalState] = useState(false);
+  const show = clsx(
+    'scroll-y-none pointer-events-auto',
+    'bg-opacity-50 bg-clip-padding backdrop-blur-sm backdrop-filter'
+  );
+  const hide = 'pointer-events-none opacity-0';
+
+  useEffect(() => {
+    const timer = setTimeout(() => setOpenLocalState(open), 300);
+    return () => clearTimeout(timer);
+  }, [open]);
+
+  return (
+    <div
+      className={clsx(
+        'overflow-y-hidden',
+        'fixed inset-0 z-50',
+        'h-screen w-screen',
+        'flex items-center justify-center',
+        'transition-all delay-100',
+        openLocalState ? show : hide
+      )}
+    >
+      <Card
+        className={clsx(
+          'h-max w-max',
+          openLocalState ? 'scale-100 opacity-100' : 'scale-0 opacity-0',
+          'transition-all delay-100 duration-200',
+          'overflow-hidden'
+        )}
+      >
+        <section className='relative flex flex-col overflow-scroll py-4'>
+          {withCloseBtn && <CloseButton handleOpen={handleOpen} />}
+          {children}
+          {withOkBtn && <OkButton handleOpen={handleOpen} />}
+        </section>
+      </Card>
+    </div>
+  );
+};
+
+const OkButton = ({ handleOpen }: { handleOpen: () => void }) => {
+  return (
+    <Button onClick={handleOpen} className='flex items-center justify-center'>
+      OK
+    </Button>
+  );
+};
+
+const CloseButton = ({ handleOpen }: { handleOpen: () => void }) => {
+  return (
+    <button
+      onClick={handleOpen}
+      className={clsx(
+        'absolute top-2 right-2',
+        'flex items-center justify-center'
+      )}
+    >
+      <AiOutlineClose className='text-3xl' />
+    </button>
+  );
+};
