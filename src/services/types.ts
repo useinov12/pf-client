54f2744b2336b83a26bc3b6b8702d71aec987946
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-15 12:49:23 -0500

    reafctor: data types updated

--- src/services/types.ts
+++ src/services/types.ts
@@ -17,7 +17,7 @@ type UserData = {
   first_name: string;
   last_name: string;
 };
-export type Error = any
+export type Error = any;
 
 /* ================== User ==================*/
 type UserApiData = Omit<UserData, 'password'>;
@@ -30,6 +30,9 @@ export type LoginCredentials = Pick<UserData, 'username' | 'password'>;
 export type LoginData = Response<Tokens>;
 export type RegisterData = Response<UserApiData>;
 export type CurrentUserData = Response<UserApiData>;
+// export type RefreshTokenData = Response<string>;
+// update when backend is ready
+export type RefreshTokenData = Response<any>;
 
 export type UserInContext = {
   firstName: string;
@@ -38,6 +41,58 @@ export type UserInContext = {
 };
 
 /* ================== Plaid ================== */
-export type GetTokenResponse = Response<{
-  link_token: string;
-}>;
+export type LinkTokenData = Response<string>;
+
+/* ================== Data ================== */
+export type Account = {
+  id: string;
+  subtype: string;
+  bank_name: string;
+  balance: number;
+  name: string;
+  user_id: number;
+};
+type BankName = string;
+export type ConnectedBanks = Record<BankName, Account[]>;
+
+
+export type ConnectedBanksData = Response<ConnectedBanks>;
+
+const banks: ConnectedBanks = {
+  'Navy Federal': [
+    {
+      id: 'nMXbrOz4BRSMDVnyZV50hpYaOvgXmvHAxvR4P',
+      subtype: 'credit card',
+      bank_name: 'Navy Federal',
+      balance: 175,
+      name: 'More Rewards Amex',
+      user_id: 6,
+    },
+    {
+      id: 'QLBP76NJAwTvarMmkr5Zs1k6OmNpRmHEKqYj6',
+      subtype: 'savings',
+      bank_name: 'Navy Federal',
+      balance: 6515,
+      name: 'Share Savings',
+      user_id: 6,
+    },
+    {
+      id: 'RDbqKLNJv6SpdmPyJmA5f70Lekrp8kHykmxR0',
+      subtype: 'checking',
+      bank_name: 'Navy Federal',
+      balance: 1253,
+      name: 'Active Duty Checking',
+      user_id: 6,
+    },
+  ],
+  'American Express': [
+    {
+      id: 'Rav3jBBy1LULQ3P8OqeZCMprNDzZDkSVoopOa',
+      subtype: 'credit card',
+      bank_name: 'American Express',
+      balance: 769,
+      name: 'MAKSYM KALINCHENKO -91008',
+      user_id: 6,
+    },
+  ],
+};
