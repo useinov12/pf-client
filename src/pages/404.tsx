eea04ee9d4f2e28fb22f810f43b55b9c657ce7ed
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-17 17:14:10 -0500

    style: bg and text color

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
-import Layout from '@/components/page/landing/layout/Layout';
+import Layout from '@/components/page/landing/Layout';
 import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
 
@@ -11,13 +11,13 @@ export default function NotFoundPage() {
       <Seo templateTitle='Not Found' />
 
       <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+        <section className=''>
+          <div className='layout flex min-h-[90vh] flex-col items-center justify-center text-center'>
             <RiAlarmWarningFill
               size={60}
               className='drop-shadow-glow animate-flicker text-red-500'
             />
-            <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
+            <h1 className='mt-8 text-3xl md:text-5xl '>Page Not Found</h1>
             <ArrowLink className='mt-4 md:text-lg' href='/'>
               Back to Home
             </ArrowLink>
