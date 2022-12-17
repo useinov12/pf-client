c84620ca2328578a89a2d19864384b65b35bb34e
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-17 17:03:41 -0500

    docs: comments

--- src/components/LoginForm/Form.tsx
+++ src/components/LoginForm/Form.tsx
@@ -9,10 +9,12 @@ import { useLoginForm } from '@/context/LoginFormProvider';
 interface SignInFormProps {
   withCloseBtn?: boolean;
 }
+
+/* SignInForm watches LoginProvider Context state to decide if open Popup component  */
 export function SignInForm({ withCloseBtn }: SignInFormProps) {
   const router = useRouter();
   const { openLoginForm, setOpenLoginForm } = useLoginForm();
-  const [togglePopup, setTogglePopup] = useState(true);
+  const [togglePopup, setTogglePopup] = useState(false);
 
   function handleTogglePopup() {
     setTogglePopup((p) => !p);
@@ -24,7 +26,7 @@ export function SignInForm({ withCloseBtn }: SignInFormProps) {
   }, []);
 
   useEffect(() => {
-    /* if navigating away from  /signup page -> close Signin Popup in context  */
+    /* if navigating away from  /signup page -> close SignIn Popup in context  */
     if (router.pathname !== '/signup') {
       setOpenLoginForm(false);
     }
@@ -53,6 +55,13 @@ interface FormPopup {
   handleTogglePopup: () => void;
   withCloseBtn?: boolean;
 }
+
+/**
+ *  Component wraps agnostic `<Popup/>` component to open a form.
+ * `<Popup/>` require external state and handler to open/close popup.
+ * `<SignUpPopup/>` provides handler from LoginProvider Context 
+ * and current `open/close` state from parent component
+ */
 const SignUpPopup = ({
   withCloseBtn,
   togglePopup,
@@ -72,7 +81,7 @@ const SignUpPopup = ({
         <span className='px-3 text-center text-sm text-gray-700'>
           Already have an account?{' '}
           <button onClick={handleTogglePopup}>
-            <strong 
+            <strong
               className='cursor-pointer text-primary-600 
               hover:border-b hover:border-primary-600'
             >
@@ -103,7 +112,7 @@ const SignInPopup = ({
         <span className='text-sm text-gray-700'>
           Don't have an account yet?{' '}
           <button onClick={handleTogglePopup}>
-            <strong 
+            <strong
               className='cursor-pointer text-center text-primary-600 
               hover:border-b hover:border-primary-600'
             >
