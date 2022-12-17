e9ce574c1fc4c9974796fb4985c0921d169384a1
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-17 15:03:22 -0500

    feat: pages for app created

---
+++ src/pages/app/banks.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/page/app/Layout';
+
+export default function BanksPage(){
+    return (
+        <Layout>
+          
+        </Layout>
+      );
+}
---
+++ src/pages/app/transactions.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/page/app/Layout";
+
+export default function TransactionsPage(){
+    return (
+        <Layout>
+          
+        </Layout>
+      );
+}
