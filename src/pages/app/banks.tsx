ad8f34491e14af04457919efa3c603b4ac3ab85d
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-17 17:08:21 -0500

    reafctor: layouts

--- src/pages/app/banks.tsx
+++ src/pages/app/banks.tsx
@@ -1,9 +1,9 @@
 import Layout from '@/components/page/app/Layout';
+import { useAppPageContext } from '@/context/AppPageContext';
 
 export default function BanksPage(){
     return (
         <Layout>
-          
         </Layout>
       );
 }
