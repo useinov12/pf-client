d5f939818cd14d6ad4ef917b36dc4466da89be06
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-17 17:06:22 -0500

    reafctor: user menu

--- src/components/page/cabinet/sections/User.tsx
+++ src/components/page/cabinet/sections/User.tsx
@@ -3,16 +3,28 @@ import clsx from 'clsx';
 import Menu from '../Menu';
 import { ThemeContext } from '@/context/ThemeProvider';
 import { useAuth } from '@/services/user/AuthProvider';
-import { FiSettings } from 'react-icons/fi';
+import { FiLogOut } from 'react-icons/fi';
+import { FaUserCircle } from 'react-icons/fa';
+import { HiDotsVertical } from 'react-icons/hi';
+import MenuHeader from '../MenuHeader';
 
 const UserMenu = () => {
   return (
     <Menu
-      className={'first-lpeer-in-range:h-full relative w-min overflow-y-scroll'}
+      className={clsx(
+        'h-min w-full',
+        'md:h-full md:w-1/4',
+        'relative h-full overflow-y-scroll'
+      )}
     >
-      <section className='flex justify-start py-3 px-7'>
-        <Header />
-        <UserProfile withSettings />
+      <section className='flex flex-col items-center justify-start md:items-start'>
+        <MenuHeader>
+          <h4>Account</h4>
+        </MenuHeader>
+        <div className='flex w-full items-center justify-between py-2 px-5 md:items-start '>
+          <UserProfile withSettings />
+          <LogoutBtn />
+        </div>
       </section>
     </Menu>
   );
@@ -20,91 +32,46 @@ const UserMenu = () => {
 
 export default UserMenu;
 
-const Header = () => {
-  const { mode } = useContext(ThemeContext);
-  return (
-    <header
-      className={clsx(
-        'w-full',
-        'sticky top-0 z-40',
-        'flex items-center justify-between px-7 py-4',
-        mode === 'light' ? 'bg-gray-300' : 'bg-gray-900'
-      )}
-    >
-      <h4>Account</h4>
-    </header>
-  );
-};
-
-const Avatar = ({ firstName }: { firstName: string }) => {
-  return (
-    <h1
-      className={clsx(
-        'text-3xl',
-        'h-9 w-9',
-        'md:text-3xl',
-        'rounded text-center',
-        'ring-4 ring-primary-600',
-        'drop-shadow'
-      )}
-    >
-      {`${firstName[0]}`}
-    </h1>
-  );
-};
-
 const UserProfile = ({ withSettings }: { withSettings?: boolean }) => {
-  const { user } = useAuth();
+  const { user, handleLogout } = useAuth();
 
   if (!user) return null;
   return (
-    <div className='flex items-center justify-center gap-2'>
-      <Avatar firstName={user.firstName} />
-      <div className='flex flex-col'>
-        <strong>{`${user.firstName} ${user?.lastName}`}</strong>
-        <p className='text-sm text-gray-500'>{user.username}</p>
+    <div className='flex gap-2 py-3 md:flex-col md:px-5'>
+      <FaUserCircle className='text-6xl' />
+      <div className='flex items-center gap-2'>
+        <div className='flex flex-col'>
+          <h4>{`${user.firstName} ${user?.lastName}`}</h4>
+          <p className='text-sm text-gray-500'>{user.username}</p>
+        </div>
+        {withSettings && <UserSettingsBtn />}
       </div>
-      {withSettings && <UserSettingsBtn />}
     </div>
   );
 };
 
 const UserSettingsBtn = () => {
   const { mode } = useContext(ThemeContext);
-  // const { handleBankConfigPopup } = useCabinetPageContext();
+  // handle popup
   return (
     <button
       className={clsx(
-        'rounded-full bg-none p-2 ',
+        'rounded-md bg-none py-1',
         mode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-500'
       )}
       onClick={() => {}}
     >
-      <FiSettings className='h-8 w-8' />
+      <HiDotsVertical className='h-8 w-8' />
     </button>
   );
 };
 
-{
-  /* <div className='flex gap-2 md:flex-col md:gap-0'>
-  <Button
-    className={clsx(
-      'flex items-center justify-center',
-      'my-1 w-36 py-1',
-      'text-sm'
-    )}
-  >
-    Change Name
-  </Button>
-  <Button
-    variant='red'
-    className={clsx(
-      'flex items-center justify-center',
-      'my-1 w-36 py-1 ',
-      'text-sm'
-    )}
-  >
-    Delete account
-  </Button>
-</div> */
-}
+const LogoutBtn = () => {
+  const { handleLogout } = useAuth();
+  return (
+    <button className='text-md' onClick={handleLogout}>
+      <FiLogOut className='text-3xl hover:text-red-500' />
+    </button>
+  );
+};
+
