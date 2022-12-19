1fd15f2a8de50fd437286bd25153a88d82e96a53
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-19 12:29:17 -0500

    reafctor: bar chart design unified

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -1,77 +1,45 @@
-import React from 'react';
-import type { ChartData } from 'chart.js';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarController,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  BarController,
-  Title,
-  Tooltip,
-  Legend
-);
-import { Bar, Chart } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
+import { useEffect, useRef, useState } from 'react';
+import { Chart } from 'react-chartjs-2';
+import { Chart as ChartJS, ChartData } from 'chart.js';
+import { getChartDataStructure } from '@/lib/chartHelpers';
+import { ChartProps } from './types';
 
-const BarChart: React.FC<{
+import 'chart.js/auto';
+
+interface BarChartProps extends ChartProps {
   width: string;
   height: string;
-  isFakeData?: boolean;
-  externalData?:number[];
-  labels?:string[];
-  delay?:number;
-}> = ({ width, height, isFakeData, externalData, delay, labels}) => {
+  stacked?: boolean | undefined;
+}
 
-  const chartRef = React.useRef<ChartJS>(null);
-  const [chartData, setChartData] = React.useState<ChartData<'line'>>({
+export default function BarChart({
+  width,
+  height,
+  delay,
+  incomingData,
+  stacked,
+}: BarChartProps) {
+  const chartRef = useRef<ChartJS>(null);
+  const [chartData, setChartData] = useState<ChartData<'line'>>({
     datasets: [],
   });
 
-  const data = {
-    // labels: labels.map((month) => month.slice(0, 3)),
-    labels: labels ? labels : months.map((month) => month.slice(0, 3)),
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: externalData ? externalData : months.map(() => 100 ),
-      },
-    ],
-  };
-
-  React.useEffect(() => {
+  useEffect(() => {
     const chart = chartRef.current;
 
-    if (!chart) {
+    if (!chart || !incomingData) {
       return;
     }
-    const chartData = {
-      ...data,
-      datasets: data.datasets.map((dataset) => ({
-        ...dataset,
-        backgroundColor: 'rgba(120, 113, 108, 1)',
-      })),
-    };
-    
-    if(delay){
-      const timer = setTimeout(()=>{
+
+    const chartData = getChartDataStructure(incomingData);
+
+    if (delay) {
+      const timer = setTimeout(() => {
         setChartData(chartData);
-      }, delay)
-      return () => clearTimeout(timer)
-    }
-    else setChartData(chartData);
-
-  }, [externalData]);
-
-  // return <div>Bar Chart</div>
+      }, delay);
+      return () => clearTimeout(timer);
+    } else setChartData(chartData);
+  }, [incomingData]);
 
   return (
     <Chart
@@ -80,38 +48,13 @@ const BarChart: React.FC<{
       data={chartData}
       width={width}
       height={height}
-      options={options}
+      options={stacked ? optionsStacked : optionsRegular}
     />
   );
-};
+}
 
-export default BarChart;
-
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  // 'September',
-  // 'October',
-  // 'November',
-  // 'December',
-];
-const colors = [
-  'red',
-  'orange',
-  'yellow',
-  'lime',
-  'green',
-  'teal',
-  'blue',
-  'purple',
-];
-export const options = {
+/* Chart JS oprtions for Bar Chart*/
+const optionsRegular = {
   responsive: true,
   maintainAspectRatio: false,
   plugins: {
@@ -135,19 +78,35 @@ export const options = {
   },
 };
 
-// const data = {
-//   months,
-//   datasets: [
-//     {
-//       //   label: 'LAbEL',
-//       data: months.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-//       backgroundColor: (context: ScriptableContext<'bar'>) => {
-//         const ctx = context.chart.ctx;
-//         const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-//         gradient.addColorStop(0, '#fde047');
-//         gradient.addColorStop(1, '#f97316');
-//         return gradient;
-//       },
-//     },
-//   ],
-// };
+const optionsStacked = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    xAxis: {
+      display: false,
+    },
+    yAxis: {
+      display: false,
+    },
+    x: {
+      stacked: true,
+      grid: {
+        color: 'transparent',
+      },
+      ticks: {
+        color: '#374151',
+      },
+    },
+    y: {
+      stacked: true,
+      grid: {
+        color: 'transparent',
+        beginAtZero: true,
+      },
+      ticks: {
+        color: '#374151',
+        beginAtZero: true,
+      },
+    },
+  },
+};
