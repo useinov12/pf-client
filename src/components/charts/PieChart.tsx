dca3338af35feb0da99440ca608e37e5b29b5fd8
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-19 12:29:42 -0500

    reafctor: pie chart design unified

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,121 +1,50 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Chart } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineController,
-  LineElement,
-  Tooltip,
-  Legend,
-  Filler,
-  ArcElement,
-  PieController
-} from 'chart.js';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  LineController,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend,
-  Filler,
-  ArcElement,
-  PieController
-);
+import { Chart as ChartJS, ChartData } from 'chart.js';
+import { ChartProps } from './types';
+import { getChartDataStructure } from '@/lib/chartHelpers';
 
-import type { ChartData, ScriptableContext } from 'chart.js';
+import 'chart.js/auto';
 
-const PieChart: React.FC<{
+interface PieChartProps extends ChartProps {
   radius: string;
-  isFakeData?: boolean;
-  externalData?: number[];
-  labels?: string[];
-  delay?: number;
-}> = ({ radius, isFakeData, externalData, labels, delay }) => {
-  const chartRef = React.useRef<ChartJS>(null);
-  const [chartData, setChartData] = React.useState<ChartData<'pie'>>({
+}
+
+export default function PieChart({
+  radius,
+  delay,
+  incomingData,
+}: PieChartProps) {
+  const chartRef = useRef<ChartJS>(null);
+  const [chartData, setChartData] = useState<ChartData<'pie'>>({
     datasets: [],
   });
 
-
-  const data = {
-    labels: labels ? labels : ['Data#1'],
-    datasets: [
-      {
-        data: externalData ? externalData : [1000],
-      },
-    ],
-  };
-
-  React.useEffect(() => {
+  useEffect(() => {
     const chart = chartRef.current;
 
-    if (!chart) {
+    if (!chart || !incomingData) {
       return;
     }
 
-    const chartData = {
-      ...data,
-      datasets: data.datasets.map((dataset) => ({
-        ...dataset,
-        backgroundColor: [
-          'rgba(214, 211, 209, 0.7)',
-          'rgba(120, 113, 108, 0.7)',
-          'rgba(68, 64, 60, 0.7)',
-          'rgba(83, 82, 82, 0.7)',
-        ],
-        borderColor: [
-          'rgba(214, 211, 209, 1)',
-          'rgba(120, 113, 108, 1)',
-          'rgba(68, 64, 60, 1)',
-          'rgba(83, 82, 82, 1)',
-        ],
-        borderWidth: 3,
-      })),
-    };
-    
+    const chartData = getChartDataStructure(incomingData);
 
+    /* if delay -> update datat with delay */
     if (delay) {
       const timer = setTimeout(() => {
         setChartData(chartData);
       }, delay);
       return () => clearTimeout(timer);
     } else setChartData(chartData);
-  }, [externalData]);
+  }, [incomingData]);
 
-
-  return (
-    // <div className='bg-green-500 w-full h-full'>
-        <Chart
-          type='pie'
-          ref={chartRef}
-          data={chartData}
-          options={options}
-          // height={'100%'}
-          // width={'100%'}
-          // className='absolute'
-        />
-    // </div>
-  );
-};
-
-export default PieChart;
-
-function createGradient({ context }: { context: ScriptableContext<'pie'> }) {
-  const ctx = context.chart.ctx;
-  const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-  gradient.addColorStop(0, 'rgba(54, 162, 235, 1)');
-  gradient.addColorStop(1, 'rgba(255, 206, 86, 1)');
-  return gradient;
+  return <Chart type='pie' ref={chartRef} data={chartData} options={options} />;
 }
 
+/* Chart JS oprtions for Pie Chart*/
 const options = {
   responsive: true,
   maintainAspectRatio: true,
-  // radius: radius,
   plugins: {
     legend: {
       display: false,
