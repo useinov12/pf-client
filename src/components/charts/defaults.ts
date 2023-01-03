de2437053e864024f63bb08ff52acf8b06624e9c
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-03 12:07:17 -0500

    reafctor: defaults update

--- src/components/charts/defaults.ts
+++ src/components/charts/defaults.ts
@@ -1,51 +1,65 @@
 export const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  
-  export const defaultColorsStroke = [
-    'rgba(63, 81, 181, 1)', // Indigo
-    'rgba(0, 150, 136, 1)', // Teal
-    'rgba(255, 193, 7, 1)', // Amber
-    'rgba(156, 39, 176, 1)', // Purple
-    'rgba(236, 64, 122, 1)', // Rose pink
-    'rgba(243, 156, 18, 1)', // Mustard yellow
-    'rgba(255, 87, 34, 1)', // Coral
-    'rgba(153, 102, 204, 1)', // Purple
-    'rgba(76, 175, 80, 1)', // Green
-    'rgba(0, 188, 212, 1)', // Turquoise
-    'rgba(121, 85, 72, 1)', // Cocoa brown
-    'rgba(189, 189, 189, 1)', // Silver
-    'rgba(205, 220, 57, 1)', // Lime
-    'rgba(139, 195, 74, 1)', // Olive
-    'rgba(255, 235, 59, 1)', // Yellow
-  ];
-  
-  export const defaultColorsBackground = [
-    'rgba(63, 81, 181, 0.4)', // Indigo
-    'rgba(0, 150, 136, 0.4)', // Teal
-    'rgba(255, 193, 7, 0.4)', // Amber
-    'rgba(153, 102, 204, 0.4)', // Purple
-    'rgba(236, 64, 122, 0.4)', // Rose pink
-    'rgba(243, 156, 18, 0.4)', // Mustard yellow
-    'rgba(255, 87, 34, 0.4)', // Coral
-    'rgba(156, 39, 176, 0.4)', // Purple
-    'rgba(76, 175, 80, 0.4)', // Green
-    'rgba(0, 188, 212, 0.4)', // Turquoise
-    'rgba(121, 85, 72, 0.4)', // Cocoa brown
-    'rgba(189, 189, 189, 0.4)', // Silver
-    'rgba(205, 220, 57, 0.4)', // Lime
-    'rgba(139, 195, 74, 0.4)', // Olive
-    'rgba(255, 235, 59, 0.4)', // Yellow
-  ];
-  
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const defaultColorsStroke = [
+  'rgba(63, 81, 181, 1)', // Indigo
+  'rgba(0, 150, 136, 1)', // Teal
+  'rgba(255, 193, 7, 1)', // Amber
+  'rgba(156, 39, 176, 1)', // Purple
+  'rgba(236, 64, 122, 1)', // Rose pink
+  'rgba(243, 156, 18, 1)', // Mustard yellow
+  'rgba(255, 87, 34, 1)', // Coral
+  'rgba(153, 102, 204, 1)', // Purple
+  'rgba(76, 175, 80, 1)', // Green
+  'rgba(0, 188, 212, 1)', // Turquoise
+  'rgba(121, 85, 72, 1)', // Cocoa brown
+  'rgba(189, 189, 189, 1)', // Silver
+  'rgba(205, 220, 57, 1)', // Lime
+  'rgba(139, 195, 74, 1)', // Olive
+  'rgba(255, 235, 59, 1)', // Yellow
+];
+
+export const defaultColorsBackground = [
+  'rgba(63, 81, 181, 0.4)', // Indigo
+  'rgba(0, 150, 136, 0.4)', // Teal
+  'rgba(255, 193, 7, 0.4)', // Amber
+  'rgba(153, 102, 204, 0.4)', // Purple
+  'rgba(236, 64, 122, 0.4)', // Rose pink
+  'rgba(243, 156, 18, 0.4)', // Mustard yellow
+  'rgba(255, 87, 34, 0.4)', // Coral
+  'rgba(156, 39, 176, 0.4)', // Purple
+  'rgba(76, 175, 80, 0.4)', // Green
+  'rgba(0, 188, 212, 0.4)', // Turquoise
+  'rgba(121, 85, 72, 0.4)', // Cocoa brown
+  'rgba(189, 189, 189, 0.4)', // Silver
+  'rgba(205, 220, 57, 0.4)', // Lime
+  'rgba(139, 195, 74, 0.4)', // Olive
+  'rgba(255, 235, 59, 0.4)', // Yellow
+];
+
+export const grayShades = [
+  'rgba(128, 128, 128, 1)', // Light Gray
+  'rgba(211, 211, 211, 1)', // Light Steel Blue
+  'rgba(169, 169, 169, 1)', // Dark Gray
+  'rgba(105, 105, 105, 1)', // Dim Gray
+  'rgba(150, 150, 150, 1)', // Medium Gray
+];
+export const grayShadesBg = [
+  'rgba(128, 128, 128, 0.7)', // Light Gray
+  'rgba(211, 211, 211, 0.7)', // Light Steel Blue
+  'rgba(169, 169, 169, 0.7)', // Dark Gray
+  'rgba(105, 105, 105, 0.7)', // Dim Gray
+  'rgba(150, 150, 150, 0.7)', // Medium Gray
+];
