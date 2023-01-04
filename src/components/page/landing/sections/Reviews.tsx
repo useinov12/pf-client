df81441483e0cb4369be625dd76a6c6562b08601
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-03 19:21:31 -0500

    reafctor: components isolated

--- src/components/page/landing/sections/Reviews.tsx
+++ src/components/page/landing/sections/Reviews.tsx
@@ -1,47 +1,70 @@
-import React from 'react';
+import { ReactNode } from 'react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { useTheme } from '@/context/ThemeProvider';
-import Path from '../Path';
-import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+import { FaQuoteRight } from 'react-icons/fa';
 import { SiCircle } from 'react-icons/si';
-import Polkadot from '../shared/Polkadot';
+import Polkadot from '../../../shared/Polkadot';
 
 export default function ReviewsSection() {
   return (
-    <article
+    <Container className='my-20 flex flex-col items-center justify-end gap-6'>
+      <Header />
+      <ReviewList />
+    </Container>
+  );
+}
+
+interface SectionWrapperProps {
+  className?: string;
+  children?: ReactNode;
+}
+
+const Container = ({ children, className }: SectionWrapperProps) => {
+  return (
+    <section
       className={clsx(
         'mx-auto mt-2 px-3',
         'sm:max-w-screen-sm ',
         'md:max-w-screen-lg ',
-        'lg:max-w-screen-xl'
+        'lg:max-w-screen-xl',
+        className
       )}
     >
-      <section className='my-20 flex flex-col items-center justify-end gap-6'>
-        <div className='flex w-full flex-col items-center gap-3 lg:flex-row'>
-          <SiCircle className=' h-16 w-16 rounded-full' />
-          <div className=''>
-            <div className='mb-10 flex w-full flex-col  items-center lg:mb-3 lg:items-start '>
-              <h2 className='cursor-default text-center text-2xl tracking-tight drop-shadow lg:text-left'>
-                App that you looking for
-              </h2>
-              <h3 className='cursor-default text-center text-lg font-normal drop-shadow lg:text-left'>
-                It was never this easy to look into your own financial state
-              </h3>
-            </div>
-          </div>
-        </div>
-        <div className='flex h-max w-full items-center justify-center sm:items-start'>
-          <ul className='grid gap-2 md:grid-cols-2  lg:grid-cols-3'>
-            {reviews.map((review, i) => (
-              <Review review={review} key={review.name} />
-            ))}
-          </ul>
-        </div>
-      </section>
-    </article>
+      {children}
+    </section>
   );
-}
+};
+
+const Header = () => {
+  return (
+    <div className='flex w-full flex-col items-center gap-3 lg:flex-row'>
+      <SiCircle className=' h-16 w-16 rounded-full' />
+      <div className=''>
+        <div className='mb-10 flex w-full flex-col  items-center lg:mb-3 lg:items-start '>
+          <h2 className='cursor-default text-center text-2xl tracking-tight drop-shadow lg:text-left'>
+            App that you looking for
+          </h2>
+          <h3 className='cursor-default text-center text-lg font-normal drop-shadow lg:text-left'>
+            It was never this easy to look into your own financial state
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ReviewList = () => {
+  return (
+    <div className='flex h-max w-full items-center justify-center sm:items-start'>
+      <ul className='grid gap-2 md:grid-cols-2  lg:grid-cols-3'>
+        {reviews.map((review, i) => (
+          <Review review={review} key={review.name} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 interface ReviewProps {
   review: {
@@ -58,24 +81,24 @@ const Review = ({ review }: ReviewProps) => {
       className={clsx(
         'relative overflow-hidden',
         'flex items-center justify-center rounded',
-        'h-40 w-full border  px-2 md:h-48  ',
+        'h-36 w-full border  px-2 ',
         mode === 'light' ? 'border-dark/50' : 'border-gray-400/50',
         mode === 'light' ? 'bg-gray-400/50' : 'bg-gray-700/50'
       )}
     >
       <Polkadot className='absolute top-0 z-0 -translate-x-52' />
-      <FaQuoteRight className='absolute top-6 right-6 text-2xl opacity-70' />
+      <FaQuoteRight className='absolute top-2 right-6 text-2xl opacity-70' />
 
-      <div className='flex w-full h-full gap-4'>
-        <div className='w-2/5 my-4'>
+      <div className='flex h-full w-full gap-4'>
+        <div className='my-2 w-2/5'>
           <Image src={review.image} width={120} height={110} />
         </div>
 
-        <div className='w-3/5 md:mt-5 flex flex-col justify-center items-center'>
-          <span className='py-2 font-semibold text-lg tracking-tight'>
+        <div className='relative flex w-3/5 flex-col items-center justify-start pt-10'>
+          <span className='text-md font-semibold tracking-tight'>
             {review.review}
           </span>
-          <h6 className='w-full px-8 text-right font-serif text-xl'>
+          <h6 className='absolute bottom-1 right-2 text-right font-serif text-xl'>
             {review.name}
           </h6>
         </div>
@@ -92,22 +115,22 @@ const reviews = [
   },
   {
     name: 'James',
-    review: 'Intuitive interface and beautiful design.',
+    review: 'Intuitive to use and beautiful design.',
     image: '/images/portraits/2.png',
   },
   {
     name: 'Noah',
-    review: 'The app is fast and realy informative!',
+    review: 'The app is realy helpful!',
     image: '/images/portraits/3.png',
   },
   {
     name: 'Emma',
-    review: 'I love the product! Definitely going to use it!',
+    review: 'Definitely going to use it!',
     image: '/images/portraits/4.png',
   },
   {
     name: 'Olivia',
-    review: 'Realy great idea and nice implementation!',
+    review: 'Great idea and nice implementation!',
     image: '/images/portraits/5.png',
   },
 ];
