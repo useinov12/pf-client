edcd235b7bf175a4a1f1d37d81e2033fb0c57605
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-05 12:33:50 -0500

    reafctor: menu components moved in one file

--- src/components/page/cabinet/Menu.tsx
+++ src/components/page/cabinet/Menu.tsx
@@ -1,20 +1,21 @@
 import { useContext } from 'react';
 import clsx from 'clsx';
-import { ThemeContext } from '@/context/ThemeProvider';
+import { useTheme } from '@/context/ThemeProvider';
 
-const MenuSection = ({
+export function MenuSection({
   className,
   children,
 }: {
   className?: string;
   children: JSX.Element | JSX.Element[];
-}) => {
-  const { mode } = useContext(ThemeContext);
+}) {
+  const { mode } = useTheme();
 
   return (
     <div
       className={clsx(
-        'rounded-md border',
+        'border-b' /*  mobile styles  */,
+        'sm:rounded-md  sm:border' /* desktop & tablet stryles */,
         mode === 'light' ? 'border-gray-100/50' : 'border-gray-300/20',
         mode === 'light' ? 'bg-dark/10' : 'bg-gray-300/10',
         'overflow-hidden drop-shadow',
@@ -24,6 +25,24 @@ const MenuSection = ({
       {children}
     </div>
   );
-};
+}
 
-export default MenuSection;
+export function MenuHeader({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) {
+  const { mode } = useTheme();
+  return (
+    <header
+      className={clsx(
+        'w-full',
+        'sticky top-0 z-40',
+        'flex items-center justify-between px-7 py-4',
+        mode === 'light' ? 'bg-gray-400' : 'bg-gray-900'
+      )}
+    >
+      {children}
+    </header>
+  );
+}
