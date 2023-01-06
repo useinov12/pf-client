6cd4cf211ada676771a04d50c24e41aecb8a9a5a
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-05 19:30:36 -0500

    reafctor: app layout strucutre refactored

--- src/components/page/app/Layout.tsx
+++ src/components/page/app/Layout.tsx
@@ -1,71 +1,77 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 import clsx from 'clsx';
 import { useTheme } from '@/context/ThemeProvider';
 import Sidebar, { MobileMenuButton } from './Sidebar';
 import ThemeButton from '@/components/shared/ThemeSwitch';
 import { useRouter } from 'next/router';
 import Logo from '@/components/shared/Logo';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/shared/Tooltip';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/shared/Tooltip';
 import { AppPageProvider } from '@/context/AppPageContext';
 import LayoutCommon from '@/components/shared/LayoutCommon';
-import { MobileSidebar } from './Sidebar';
 
-export default function Layout({ children, className }: { children: ReactNode, className?:string  }) {
-  const { mode } = useTheme();
-
+export default function Layout({
+  children,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
     <AppPageProvider>
       <LayoutCommon withWrapper className='h-screen'>
         <Navbar />
-        <main
-          className={clsx(
-            'relative flex h-5/6 w-full',
-            'gap-2 overflow-hidden px-1',
-            'transition-all duration-75',
-            'transform-gpu'
-          )}
-        >
-          <Sidebar className='hidden md:block' />
-          <MobileSidebar />
-          <MainSection className={className}>{children}</MainSection>
-        </main>
+        <MainSection>{children}</MainSection>
       </LayoutCommon>
     </AppPageProvider>
   );
 }
 
-const MainSection = ({ children, className }: { children: ReactNode, className?:string }) => {
+function MainSection({ children }: { children: ReactNode }) {
+  const { mode } = useTheme();
+  return (
+    <section
+      className={clsx(
+        'h-[85vh] w-full',
+        'border-t border-b sm:rounded-md sm:border',
+        mode === 'light' ? 'border-gray-100/50' : 'border-gray-300/20',
+        mode === 'light' ? 'bg-dark/10' : 'bg-gray-300/10',
+        'overflow-hidden drop-shadow'
+      )}
+    >
+      <SectionHeader />
+      <div className='flex h-full w-full'>
+        <Sidebar />
+        <main className='z-0 ml-20'>{children}</main>
+      </div>
+    </section>
+  );
+}
+
+function SectionHeader() {
   const { mode } = useTheme();
   const router = useRouter();
   const location = router.asPath.slice(5).toUpperCase();
   return (
-    <section
+    <div
       className={clsx(
-        'w-screen rounded border',
-        mode === 'light' ? 'border-dark/50' : 'border-gray-300/50',
-        'rounded-md border',
-        mode === 'light' ? 'border-gray-100/50' : 'border-gray-300/20',
-        mode === 'light' ? 'bg-dark/10' : 'bg-gray-300/10',
-        'overflow-hidden drop-shadow',
-        'p-5',
-        className
+        'border-b',
+        'flex items-center justify-between px-5 py-3',
+        mode === 'light' ? 'bg-gray-400' : 'bg-gray-900',
+        mode === 'light' ? 'border-gray-500/50'  : 'border-gray-300/20'
       )}
     >
-      <div className='flex items-center justify-between px-2 md:hidden'>
-        <MobileMenuButton />
-        <h2>{location}</h2>
-      </div>
-      {children}
-    </section>
+      <MobileMenuButton />
+      <strong className='text-xl'>{location}</strong>
+    </div>
   );
-};
+}
 
-const Navbar = () => {
-  const router = useRouter();
-  const location = router.asPath.slice(5).toUpperCase();
-
+function Navbar() {
   return (
-    <header className='flex items-center justify-between py-3 px-2'>
+    <header className='flex items-center justify-between py-1 px-2'>
       <div className='flex items-center justify-center gap-10'>
         <Tooltip placement='right-start'>
           <TooltipTrigger>
@@ -75,8 +81,6 @@ const Navbar = () => {
             <p>Landing page</p>
           </TooltipContent>
         </Tooltip>
-
-        <strong className='hidden md:block'>{location}</strong>
       </div>
 
       <nav className='flex items-center gap-2'>
@@ -84,4 +88,4 @@ const Navbar = () => {
       </nav>
     </header>
   );
-};
+}
