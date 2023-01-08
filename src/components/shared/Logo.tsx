62927f207e48d3c0efbb1cce815ee632a94f1ebb
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-07 21:14:59 -0500

    reafctor: classname prop added

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import clsx from 'clsx';
 
-const Logo = ({ width, height }: { width?: number; height?: number }) => {
+const Logo = ({ width, height, className }: { width?: number; height?: number, className?:string }) => {
   return (
-    <Link href='/'>
-      <Image
-        src={'/images/logo.png'}
-        width={width ? width : 68}
-        height={height ? height : 58}
-        className='cursor-pointer'
-        alt='logo'
-      />
-    </Link>
+    <div className={clsx('cursor-pointer', 'flex justify-center items-center', className)}>
+      <Link href='/'>
+        <Image
+          src={'/images/logo.png'}
+          width={width ? width : 68}
+          height={height ? height : 58}
+          alt='logo'
+        />
+      </Link>
+    </div>
   );
 };
 
