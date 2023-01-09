03728bcd4c584e6a678201855e6f501a183dfde5
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-08 20:07:58 -0500

    chore: tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,7 @@ const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   plugins: [
-    plugin(function ({ addUtilities }) {
-      addUtilities({
-        /* Hide scrollbar for Chrome, Safari and Opera */
-        '.no-scrollbar ::-webkit-scrollbar': {
-          display: 'none',
-        },
-
-        /* Hide scrollbar for IE, Edge and Firefox */
-        '.no-scrollbar': {
-          '-ms-overflow-style': 'none' /* IE and Edge */,
-          'scrollbar-width': 'none' /* Firefox */,
-        },
-      });
-    }),
+    require('tailwind-scrollbar-hide')
   ],
 
   theme: {
