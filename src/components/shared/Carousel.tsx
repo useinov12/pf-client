167f6134828d47832698db7f1ba4dd6994a3165e
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-08 20:07:09 -0500

    style: carousel style update

--- src/components/shared/Carousel.tsx
+++ src/components/shared/Carousel.tsx
@@ -10,25 +10,25 @@ import {
  *  */
 export function Carousel({ children }: { children: ReactElement[] }) {
   const [activeIndex, setActiveIOndex] = useState(0);
-  const amountOfChildrens = Children.count(children);
 
   function updateIndex(newIndex: number) {
     if (newIndex < 0) {
       newIndex = 0;
-    } else if (newIndex >= Math.floor(amountOfChildrens / 2)) {
-      newIndex = Math.floor(amountOfChildrens / 2);
+    } else if (newIndex >= Children.count(children)) {
+      newIndex = Children.count(children) - 1;
     }
     setActiveIOndex(newIndex);
   }
 
   return (
-    <section className='group relative w-[93%] overflow-hidden p-1 '>
+    <section className='group relative w-full overflow-hidden p-1 hover:bg-dark/10'>
       <button
         className={clsx(
           'absolute z-40',
-          'top-0 left-0 h-full rounded px-4',
+          // 'group-hover:opacity-100 xl:opacity-0',
+          'top-0 left-0 h-full rounded px-2',
           'group-hover:bg-dark/20',
-          'opacity-0 group-hover:opacity-100',
+          'group-focus:bg-dark/20',
           'scale-100 hover:scale-[1.1]',
           'transition-all duration-150 ease-in-out'
         )}
@@ -40,13 +40,16 @@ export function Carousel({ children }: { children: ReactElement[] }) {
       </button>
 
       <ul
-        style={{ transform: `translateX(-${activeIndex * 12}%)` }}
+        style={{
+          transform: `translateX(-${activeIndex * Children.count(children)}%)`,
+        }}
         className={clsx(
           'list-none',
           'z-40',
           'inline-flex gap-2',
           'whitespace-nowrap',
-          'transition-transform duration-500'
+          'transition-transform duration-500',
+          'overflow-x-scroll'
         )}
       >
         {Children.map(children, (child, index) => {
@@ -56,9 +59,11 @@ export function Carousel({ children }: { children: ReactElement[] }) {
       <button
         className={clsx(
           'absolute z-40',
+          // 'group-hover:opacity-100 xl:opacity-0',
+          'bg-dark/20',
           'group-hover:bg-dark/20',
-          'top-0 right-0 h-full rounded px-4',
-          'opacity-0 group-hover:opacity-100',
+          'group-focus:bg-dark/20',
+          'top-0 right-0 h-full rounded px-2',
           'scale-100 hover:scale-[1.1]',
           'transition-all duration-150 ease-in-out'
         )}
