790ba9f3b8de3e99b842cb9a46a43ab6c3f11fbe
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-09 18:00:57 -0500

    style: only one color left

--- src/components/charts/defaults.ts
+++ src/components/charts/defaults.ts
@@ -15,38 +15,38 @@ export const months = [
 
 export const defaultColorsStroke = [
   'rgba(63, 81, 181, 1)', // Indigo
-  'rgba(0, 150, 136, 1)', // Teal
-  'rgba(255, 193, 7, 1)', // Amber
-  'rgba(156, 39, 176, 1)', // Purple
-  'rgba(236, 64, 122, 1)', // Rose pink
-  'rgba(243, 156, 18, 1)', // Mustard yellow
-  'rgba(255, 87, 34, 1)', // Coral
-  'rgba(153, 102, 204, 1)', // Purple
-  'rgba(76, 175, 80, 1)', // Green
-  'rgba(0, 188, 212, 1)', // Turquoise
-  'rgba(121, 85, 72, 1)', // Cocoa brown
-  'rgba(189, 189, 189, 1)', // Silver
-  'rgba(205, 220, 57, 1)', // Lime
-  'rgba(139, 195, 74, 1)', // Olive
-  'rgba(255, 235, 59, 1)', // Yellow
+  // 'rgba(0, 150, 136, 1)', // Teal
+  // 'rgba(255, 193, 7, 1)', // Amber
+  // 'rgba(156, 39, 176, 1)', // Purple
+  // 'rgba(236, 64, 122, 1)', // Rose pink
+  // 'rgba(243, 156, 18, 1)', // Mustard yellow
+  // 'rgba(255, 87, 34, 1)', // Coral
+  // 'rgba(153, 102, 204, 1)', // Purple
+  // 'rgba(76, 175, 80, 1)', // Green
+  // 'rgba(0, 188, 212, 1)', // Turquoise
+  // 'rgba(121, 85, 72, 1)', // Cocoa brown
+  // 'rgba(189, 189, 189, 1)', // Silver
+  // 'rgba(205, 220, 57, 1)', // Lime
+  // 'rgba(139, 195, 74, 1)', // Olive
+  // 'rgba(255, 235, 59, 1)', // Yellow
 ];
 
 export const defaultColorsBackground = [
   'rgba(63, 81, 181, .4)', // Indigo
-  'rgba(0, 150, 136, .4)', // Teal
-  'rgba(255, 193, 7, .4)', // Amber
-  'rgba(156, 39, 176, .4)', // Purple
-  'rgba(236, 64, 122, .4)', // Rose pink
-  'rgba(243, 156, 18, .4)', // Mustard yellow
-  'rgba(255, 87, 34, .4)', // Coral
-  'rgba(153, 102, 204, .4)', // Purple
-  'rgba(76, 175, 80, .4)', // Green
-  'rgba(0, 188, 212, .4)', // Turquoise
-  'rgba(121, 85, 72, .4)', // Cocoa brown
-  'rgba(189, 189, 189, .4)', // Silver
-  'rgba(205, 220, 57, .4)', // Lime
-  'rgba(139, 195, 74, .4)', // Olive
-  'rgba(255, 235, 59, .4)', // Yellow
+  // 'rgba(0, 150, 136, .4)', // Teal
+  // 'rgba(255, 193, 7, .4)', // Amber
+  // 'rgba(156, 39, 176, .4)', // Purple
+  // 'rgba(236, 64, 122, .4)', // Rose pink
+  // 'rgba(243, 156, 18, .4)', // Mustard yellow
+  // 'rgba(255, 87, 34, .4)', // Coral
+  // 'rgba(153, 102, 204, .4)', // Purple
+  // 'rgba(76, 175, 80, .4)', // Green
+  // 'rgba(0, 188, 212, .4)', // Turquoise
+  // 'rgba(121, 85, 72, .4)', // Cocoa brown
+  // 'rgba(189, 189, 189, .4)', // Silver
+  // 'rgba(205, 220, 57, .4)', // Lime
+  // 'rgba(139, 195, 74, .4)', // Olive
+  // 'rgba(255, 235, 59, .4)', // Yellow
 ];
 
 export const grayShades = [
