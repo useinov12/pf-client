c4f078b34fc8ddc4bad3cbb2378b7dd846cd6225
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-12 11:21:14 -0500

    style: banks layout

--- src/pages/app/banks.tsx
+++ src/pages/app/banks.tsx
@@ -1,9 +1,33 @@
+import clsx from 'clsx';
 import Layout from '@/components/page/app/Layout';
 import { useAppPageContext } from '@/context/AppPageContext';
+import {
+  AccountsSection,
+  StatisticSection,
+  ListOfBanks,
+} from '@/components/page/app/Banks';
+import { useState } from 'react';
 
-export default function BanksPage(){
-    return (
-        <Layout>
-        </Layout>
-      );
+export default function BanksPage() {
+  const { openSidebar } = useAppPageContext();
+  const [currentBank, setCurrentBank] = useState(null);
+
+  return (
+    <Layout>
+      <section
+        className={clsx(
+          'pt-8 md:px-3',
+          'flex h-full flex-col gap-4 ',
+          openSidebar ? 'w-full md:w-[88.2%]' : 'w-full md:w-[95.8%]',
+          'overflow-y-scroll'
+        )}
+      >
+        <ListOfBanks setCurrentBank={setCurrentBank}/>
+        <div className='my-1 flex flex-col gap-2 md:flex-row'>
+          <AccountsSection />
+          <StatisticSection />
+        </div>
+      </section>
+    </Layout>
+  );
 }
