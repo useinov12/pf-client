515f072a170c52d6c2889300f6ace3572eecb2d7
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-14 16:29:13 -0500

    style: minor changes

--- src/pages/app/overview.tsx
+++ src/pages/app/overview.tsx
@@ -12,8 +12,6 @@ import { useAppPageContext } from '@/context/AppPageContext';
 
 export default function OverviewPage() {
   const { openSidebar } = useAppPageContext();
-  const router = useRouter();
-  const location = router.asPath.slice(5);
   return (
     <Layout>
       <section
@@ -24,8 +22,7 @@ export default function OverviewPage() {
           'overflow-y-scroll'
         )}
       >
-        {/* <h4 className='px-2 py-1 text-2xl font-bold capitalize'>{location}</h4> */}
-        <div className={clsx('my-1 flex flex-col gap-2 md:flex-row')}>
+        <div className='my-1 flex flex-col gap-2 md:flex-row'>
           <GeneralInfo className='w-full md:w-1/3' />
           <ChartGroup className='h-full w-full md:w-2/3' />
         </div>
