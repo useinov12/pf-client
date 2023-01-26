2271e14f723d4cdfeb683b8d2fba56cded350ecd
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-26 17:15:31 -0500

    refactor: 1. Comments added 2. Ticks formatter for scale with dollar value ticks isolated

--- src/lib/chartHelpers.ts
+++ src/lib/chartHelpers.ts
@@ -2,7 +2,6 @@ import type { ChartArea, ScriptableContext, Chart } from 'chart.js';
 import {
   StyleOptions,
   ChartDataFormat,
-  ChartDataStructure,
   Dataset,
 } from '@/components/charts/types';
 import {
@@ -11,8 +10,38 @@ import {
   grayShades,
   grayShadesBg,
 } from '@/components/charts/defaults';
+import { shortSumFormatter } from './sharedUtils';
 
-/* Creates a gradient background for Chart */
+/**
+ *  Formats incoming dataset into Chart JS dataset format
+ */
+export function getChartDataStructure({
+  incomingData,
+  chartStyles,
+  chart,
+}: {
+  incomingData: ChartDataFormat;
+  chartStyles: StyleOptions;
+  chart: Chart;
+}) {
+  const datasets: Dataset[] = incomingData.datasets.map((dataset, i) => {
+    return {
+      label: incomingData.label,
+      data: dataset,
+      backgroundColor:
+        chartStyles === 'APP' ? defaultColorsBackground : grayShadesBg,
+      borderColor: chartStyles === 'APP' ? defaultColorsStroke : grayShades,
+      borderWidth: 1,
+    };
+  });
+
+  return {
+    labels: incomingData.labels,
+    datasets: datasets,
+  };
+}
+
+/**  Creates a gradient background for Chart */
 export default function createGradient(
   ctx: CanvasRenderingContext2D | null,
   area: ChartArea
@@ -33,79 +62,31 @@ export default function createGradient(
   return 'rgba(253, 224, 71, .6)';
 }
 
-/* Formats incoming dataseet into Chart Js data for charts */
-interface GetChartStructureProps {
-  incomingData: ChartDataFormat;
-  chartStyles: StyleOptions;
-  chart: Chart;
-}
-export function getChartDataStructure({
-  incomingData,
-  chartStyles,
-  chart,
-}: GetChartStructureProps) {
-  const datasets: Dataset[] = incomingData.datasets.map((dataset, i) => {
-    return {
-      label: incomingData.label,
-      data: dataset,
-      backgroundColor:
-        chartStyles === 'APP' ? defaultColorsBackground : grayShadesBg,
-      borderColor: chartStyles === 'APP' ? defaultColorsStroke : grayShades,
-      borderWidth: 1,
-    };
-  });
+/**
+ * dollar ticks:
+ *  - add dollar sign
+ *  - format sum to short variant(1200 -> 1.2k)
+ *  - skip every second tick instance on the scale [1,2,3,4,5] -> [1,3,5]
+ */
+export const dollarTicks = {
+  // color: '#C0C0C0',
+  // Include a dollar sign in the ticks
+  callback: (value: string | number, index: number, ticks: any) => {
+    return index % 2 === 0 ? '$' + shortSumFormatter.format(Number(value)) : '';
+  },
+};
 
-  return {
-    labels: incomingData.labels,
-    datasets: datasets,
-  };
-}
-
-const labels = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
-];
-
+// example applying gradient
 const fakeDataset = {
-  labels,
+  labels: [1, 2, 3, 4, 5],
   datasets: [
     {
       label: 'Checking Account',
-      data: labels.map(() => [1, 2, 3, 43, 4]),
+      data: [1, 2, 3, 4, 5].map(() => [1, 2, 3, 43, 4]),
       backgroundColor: ({ chart: { ctx } }: ScriptableContext<'bar'>) => {
         const gradient = ctx.createLinearGradient(0, 0, 0, 200);
         gradient.addColorStop(0, '#fde047');
         gradient.addColorStop(1, '#f97316');
-        return gradient;
-      },
-    },
-    {
-      label: 'Saving Account',
-      data: labels.map(() => [1, 2, 3, 43, 4]),
-      backgroundColor: ({ chart: { ctx } }: ScriptableContext<'bar'>) => {
-        const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-        gradient.addColorStop(0, '#bae6fd');
-        gradient.addColorStop(1, '#2563eb');
-        return gradient;
-      },
-    },
-    {
-      label: 'Credit Account',
-      data: labels.map(() => [1, 2, 3, 43, 4]),
-      backgroundColor: ({ chart: { ctx } }: ScriptableContext<'bar'>) => {
-        const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-        gradient.addColorStop(0, '#a78bfa');
-        gradient.addColorStop(1, '#c026d3');
         return gradient;
       },
     },
