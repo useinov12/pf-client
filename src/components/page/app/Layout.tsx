d5ba0c1aa7b4c8ffb312fecde19d075e572a4719
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-26 13:34:08 -0500

    refactor: Desktop sidebar removed Navigation added to Navbar Mobile sidebar styles updated

--- src/components/page/app/Layout.tsx
+++ src/components/page/app/Layout.tsx
@@ -9,9 +9,17 @@ import Link from 'next/link';
 import Button from '@/components/buttons/Button';
 import { RiUserSettingsFill } from 'react-icons/ri';
 
+import { RiArrowDropRightLine } from 'react-icons/ri';
+import { HiMenuAlt1 } from 'react-icons/hi';
+import { RiApps2Line } from 'react-icons/ri';
+import { TbBuildingBank } from 'react-icons/tb';
+import { MdOutlineSwitchAccount } from 'react-icons/md';
+import { CgArrowsExchange } from 'react-icons/cg';
+import { useRouter } from 'next/router';
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <LayoutCommon className='h-screen w-screen 2xl:py-4' withWrapper>
+    <LayoutCommon className='h-screen w-screen' withWrapper>
       <ContentWrapper>
         <Navbar />
         <main className='flex h-[90vh] w-full'>
@@ -22,7 +30,6 @@ export default function Layout({ children }: { children: ReactNode }) {
     </LayoutCommon>
   );
 }
-
 function ContentWrapper({ children }: { children: ReactNode }) {
   const { mode } = useTheme();
 
@@ -31,9 +38,9 @@ function ContentWrapper({ children }: { children: ReactNode }) {
       className={clsx(
         'overflow-x-hidden overflow-y-scroll',
         'h-full w-full',
-        '2xl:rounded 2xl:border',
+        '2xl:border-l 2xl:border-r',
         mode === 'light' ? 'border-gray-400/50' : 'border-gray-300/20',
-        mode === 'light' ? 'bg-gray-200' : 'bg-gray-900'
+        mode === 'light' ? 'bg-gray-300' : 'bg-gray-900'
       )}
     >
       {children}
@@ -43,44 +50,89 @@ function ContentWrapper({ children }: { children: ReactNode }) {
 
 function Navbar() {
   const { mode } = useTheme();
+
+  const router = useRouter();
+  const location = router.asPath;
   return (
     <div
       className={clsx(
-        'h-20',
-        'border-b',
-        'flex items-center justify-between px-3 py-2',
-        mode === 'light' ? 'border-gray-500/50' : 'border-gray-300/20'
+        'px-5 pt-3',
+        'flex items-center justify-between ',
+        // 'border-b',
+        mode === 'light' ? 'border-gray-600/50' : 'border-gray-300/20'
       )}
     >
-      <div className='inline-flex items-center gap-1'>
+      <div className='inline-flex items-center gap-20'>
         <MobileMenuButton />
-        <div className='inline-flex items-center gap-2'>
+        <div className='flex items-center justify-center'>
           <Logo
-            width={62}
-            height={56}
+            width={55}
+            height={50}
             className='scale-100 hover:scale-[1.03]'
           />
-          <h3 className='mt-2 hidden sm:block'>Personal Finance</h3>
         </div>
+        <nav className='hidden md:flex'>
+          <ul className='flex items-center gap-1'>
+            {list.map((link) => (
+              <li key={link.title}>
+                <Link href={link.path}>
+                  <div
+                    className={clsx(
+                      'flex flex-col items-center  justify-center px-2 font-semibold',
+                      mode === 'light'
+                        ? location === link.path
+                          ? ' text-gray-900'
+                          : 'text-gray-800'
+                        : location === link.path
+                        ? 'text-white'
+                        : 'text-gray-500',
+                      mode === 'light'
+                        ? location !== link.path && 'hover:text-gray-700'
+                        : location !== link.path && 'hover:text-gray-300',
+                      'cursor-pointer'
+                    )}
+                  >
+                    <span className='text-2xl'>{link.icon}</span>
+                    <span className='text-sm'>{link.title}</span>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
       <div className='inline-flex items-center gap-3'>
-        <CabinetLink />
+        {/* <CabinetLink /> */}
         <ThemeSwitch />
       </div>
     </div>
   );
 }
 
-const CabinetLink = () => {
-  return (
-    <Link href='/cabinet'>
-      <Button
-        variant='transparent'
-        className='text-md inline-flex items-center gap-1 py-1'
-      >
-        <RiUserSettingsFill className='text-3xl sm:text-2xl' />
-        <span className='hidden sm:block'>Cabinet</span>
-      </Button>
-    </Link>
-  );
-};
+const list = [
+  {
+    icon: <RiApps2Line />,
+    title: 'Overview',
+    path: '/app/overview',
+  },
+  {
+    icon: <TbBuildingBank />,
+    title: 'Banks',
+    path: '/app/banks',
+  },
+  {
+    icon: <MdOutlineSwitchAccount />,
+    title: 'Accounts',
+    path: '/app/accounts',
+  },
+  {
+    icon: <CgArrowsExchange />,
+    title: 'Transactions',
+    path: '/app/transactions',
+  },
+  {
+    icon: <RiUserSettingsFill />,
+    title: 'Cabinet',
+    path: '/cabinet',
+  },
+];
--- src/components/page/app/Sidebar.tsx
+++ src/components/page/app/Sidebar.tsx
@@ -1,26 +1,20 @@
 import clsx from 'clsx';
 import { useTheme } from '@/context/ThemeProvider';
-import { MdOutlineSwitchAccount, MdGridView } from 'react-icons/md';
-import { RiBankFill } from 'react-icons/ri';
-import { CgArrowsExchange } from 'react-icons/cg';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/shared/Tooltip';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { useAppPageContext } from '@/context/AppPageContext';
 import { RiArrowDropRightLine } from 'react-icons/ri';
 import { HiMenuAlt1 } from 'react-icons/hi';
-import { RiApps2Line } from 'react-icons/ri';
+import { RiApps2Line, RiUserSettingsFill } from 'react-icons/ri';
 import { TbBuildingBank } from 'react-icons/tb';
-import { useRouter } from 'next/router';
+import { MdOutlineSwitchAccount } from 'react-icons/md';
+import { CgArrowsExchange } from 'react-icons/cg';
 
-export default function Sidebar({ className }: { className?: string }) {
+export default function Sidebar() {
   return (
     <>
       <MobileSidebar className='block md:hidden' />
-      <DesktopSidebar className='hidden md:block' />
+      {/* <DesktopSidebar className='hidden md:block' /> */}
     </>
   );
 }
@@ -36,13 +30,13 @@ function MobileSidebar({ className }: { className: string }) {
       className={clsx(
         'h-[90vh] w-full',
         'top-15 absolute z-50',
-        'transition-all duration-200 ease-in',
+        'ease transition-all duration-300',
         openSidebar && '-translate-x-[105%]',
         mode === 'light' ? 'bg-gray-200' : 'bg-gray-900',
         className
       )}
     >
-      <ul className='my-3 w-full'>
+      <ul className='my-3 mt-20 flex h-full w-full flex-col justify-start gap-3'>
         {list.map((item, i) => (
           <li
             key={item.title}
@@ -55,8 +49,8 @@ function MobileSidebar({ className }: { className: string }) {
             <Link href={item.link}>
               <div className='flex h-full w-full items-center gap-2'>
                 <>
-                  <span>{item.icon}</span>
-                  <strong>{item.title}</strong>
+                  <span className='text-3xl'>{item.icon}</span>
+                  <h1>{item.title}</h1>
                 </>
               </div>
             </Link>
@@ -67,96 +61,13 @@ function MobileSidebar({ className }: { className: string }) {
   );
 }
 
-const DesktopSidebar = ({ className }: { className: string }) => {
-  const { mode } = useTheme();
-  const { openSidebar } = useAppPageContext();
-  return (
-    <ul
-      className={clsx(
-        // 'flex-none',
-        'h-full w-fit',
-        'py-3',
-        'border-r',
-        mode === 'light' ? 'border-gray-500/50' : 'border-gray-300/20',
-        openSidebar && mode === 'light' && 'bg-gray-200',
-        openSidebar && mode === 'dark' && 'bg-gray-900',
-        className
-      )}
-    >
-      {list.map((item, i) => (
-        <li key={item.title}>
-          <MenuItem item={item} open={openSidebar} />
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-interface MenuItemProps {
-  item: { icon: JSX.Element; title: string; link: string };
-  open: boolean;
-}
-function MenuItem({ item, open }: MenuItemProps) {
-  return (
-    <div
-      className={clsx(
-        'overflow-hidden',
-        open ? 'w-40' : 'w-14' /* control width of item wrapper */
-      )}
-    >
-      <ItemWithTooltip item={item} open={open} />
-    </div>
-  );
-}
-
-function ItemWithTooltip({ item, open }: MenuItemProps) {
-  return (
-    <Tooltip placement='right-start' open={open ? false : undefined}>
-      <TooltipTrigger>
-        <Item item={item} open={open} />
-      </TooltipTrigger>
-      <TooltipContent className='rounded bg-gray-600 p-1 text-sm text-white '>
-        <p>{item.title}</p>
-      </TooltipContent>
-    </Tooltip>
-  );
-}
-
-function Item({ item, open }: MenuItemProps) {
-  const router = useRouter();
-
-  const location = router.asPath.slice(5);
-
-  return (
-    <Link href={item.link}>
-      <div
-        className={clsx(
-          'transition-all duration-300',
-          'flex items-center gap-2 py-1 px-4',
-          'hover:cursor-pointer hover:bg-gray-400/20',
-          location === item.title.toLowerCase() && 'bg-gray-400/20'
-        )}
-      >
-        <span className='text-2xl'>{item.icon}</span>
-        <p
-          className={clsx(
-            'text-left text-sm',
-            open && '',
-            open ? 'visible w-40' : 'invisible w-0' /* control width of item */,
-            location === item.title.toLowerCase() && 'font-semibold'
-          )}
-        >
-          {item.title}
-        </p>
-      </div>
-    </Link>
-  );
-}
-
 export const MobileMenuButton = () => {
   const { hanldeSidebar, openSidebar } = useAppPageContext();
   return (
-    <button onClick={hanldeSidebar} className='inline-flex items-center py-2'>
+    <button
+      onClick={hanldeSidebar}
+      className='inline-flex items-center py-2 md:hidden'
+    >
       <HiMenuAlt1 className='text-3xl' />
       <RiArrowDropRightLine
         className={clsx(
@@ -190,9 +101,9 @@ const list = [
     title: 'Transactions',
     link: '/app/transactions',
   },
-  // {
-  //   icon: <RiUserSettingsFill />,
-  //   title: 'Cabinet',
-  //   link: '/cabinet',
-  // },
+  {
+    icon: <RiUserSettingsFill />,
+    title: 'Cabinet',
+    link: '/cabinet',
+  },
 ];
