da8b506e8bcfbfaf2bc7e3c887de5dfa13be574a
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-05 12:43:55 -0500

    App Layout styles on wide screens update

--- src/components/page/app/Layout.tsx
+++ src/components/page/app/Layout.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from 'react';
 import clsx from 'clsx';
-import { useTheme } from '@/context/ThemeProvider';
-import { Navbar, MobileSidebar } from './Navbar';
+import { ReactNode } from 'react';
+
 import LayoutCommon from '@/components/shared/LayoutCommon';
 
+import { MobileSidebar, Navbar } from './Navbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -19,17 +19,9 @@ export default function Layout({ children }: { children: ReactNode }) {
   );
 }
 function ContentWrapper({ children }: { children: ReactNode }) {
-  const { mode } = useTheme();
-
   return (
     <section
-      className={clsx(
-        'overflow-x-hidden overflow-y-scroll',
-        'h-full w-full',
-        '2xl:border-l 2xl:border-r',
-        mode === 'light' ? 'border-gray-400/50' : 'border-gray-300/20',
-        mode === 'light' ? 'bg-gray-300' : 'bg-gray-900'
-      )}
+      className={clsx('overflow-x-hidden overflow-y-scroll', 'h-full w-full')}
     >
       {children}
     </section>
