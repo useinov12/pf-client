56a535c8d2ddb88f9963ab74c72af1f9ba399608
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-13 12:44:04 -0500

    refactor: page protection updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,52 +1,30 @@
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
-import React, { createContext, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-// import { AuthGuard } from '@/services/auth/AuthGuard';
-import { demoData, InitialData } from '@/constant/demo-data/demoData';
-// import { AppPageProvider } from '@/context/AppPageContext';
-// import { LoginFormProvider } from '@/context/LoginFormProvider';
+import '@/styles/globals.css';
+
+import { SignInForm as SignInFormPopup } from '@/components/LoginForm/Form';
+
+import { LoginFormProvider } from '@/context/LoginFormProvider';
 import ThemeProvider from '@/context/ThemeProvider';
 
 const queryClient = new QueryClient();
 
-export type NextApplicationPage<P = any, IP = P> = NextPage<P, IP> & {
-  requireAuth?: boolean;
-};
-
-import '@/styles/globals.css';
-
-import { LoginFormProvider } from '@/context/LoginFormProvider';
-import { useAuth } from '@/services/auth/queries';
-
 export default function MyApp(props: AppProps) {
-  const {
-    Component,
-    pageProps,
-  }: { Component: NextApplicationPage; pageProps: any } = props as AppProps;
+  const { Component, pageProps }: { Component: NextPage; pageProps: any } =
+    props as AppProps;
 
   return (
     <>
-      <Toaster />
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
-          {/* {Component.requireAuth ? (
-            <AuthGuard>
-              <Component {...pageProps} />
-            </AuthGuard>
-          ) : (
-            <LoginFormProvider>
-              <AppPageProvider>
-                <Component {...pageProps} />
-              </AppPageProvider>
-            </LoginFormProvider>
-          )} */}
           <LoginFormProvider>
-            {/* <AppRoot> */}
-            <Component {...pageProps} />
-            {/* </AppRoot> */}
+            <App>
+              <Component {...pageProps} />
+            </App>
           </LoginFormProvider>
         </QueryClientProvider>
       </ThemeProvider>
@@ -54,40 +32,12 @@ export default function MyApp(props: AppProps) {
   );
 }
 
-function AppRoot(props: { children: ReactNode }) {
-  const { data: user, isSuccess } = useAuth();
-  const isLoggedIn = isSuccess && user;
-
+function App({ children }: { children: ReactNode }) {
   return (
     <>
-      {isLoggedIn ? <PrivateVersion {...props} /> : <DemoVersion {...props} />}
+      {children}
+      <Toaster />
+      <SignInFormPopup withCloseBtn />
     </>
   );
 }
-
-function PrivateVersion({ children }: { children: ReactNode }) {
-  // ! connect total monthly balance change api call
-  // ! connect total monthly balance change BY_ACC_TYPE api call
-  // const { data, isLoading, isSuccess, isError } = useConnectedBanks();
-
-  // if (isLoading) return <Loading />;
-
-  // if (isError) return <h1>Error</h1>;
-
-  return (
-    <DataContext.Provider value={{ data: demoData /* switch to query data */ }}>
-      {children}
-    </DataContext.Provider>
-  );
-}
-function DemoVersion({ children }: { children: ReactNode }) {
-  return (
-    <DataContext.Provider value={{ data: demoData }}>
-      {children}
-    </DataContext.Provider>
-  );
-}
-
-const DataContext = createContext<{ data: InitialData }>({
-  data: demoData,
-});
--- src/pages/app/overview.tsx
+++ src/pages/app/overview.tsx
@@ -1,20 +1,15 @@
 import clsx from 'clsx';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+import usePageProtection from '@/hooks/usePageProtection';
 
 import GeneralInfo from '@/components/page/app/general/GeneralInfo';
 import Summary from '@/components/page/app/general/Summary';
 import Layout from '@/components/page/app/Layout';
-import Loading from '@/components/shared/Loading';
+import { Spinner } from '@/components/shared/Loading';
 
 import { demoData, InitialData } from '@/constant/demo-data/demoData';
 import { useAuth } from '@/services/auth/queries';
-
-//  TODO: review & refactor page rendering logic
-//  TODO: usePathValidation() refactor
-//  TODO: add login popup
-//  TODO: add login popup
-//! TODO: resolve conditional data loading: real/demo
 
 export default function OverviewPage() {
   const { pageIsValidated } = usePageProtection();
@@ -26,10 +21,11 @@ export default function OverviewPage() {
   const isLoggedIn = isSuccess && user;
 
   return isLoggedIn ? <OverviewPrivate /> : <OverviewPublic />;
+  // return <OverviewSkeleton />;
 }
 
 function OverviewPrivate() {
-  // ! add temporary demoData for missing real api parts
+  //  add temporary demoData for missing real api parts
   // const data = useConnectedBanks()
   return <Overview data={demoData} />;
 }
@@ -46,63 +42,32 @@ function Overview({ data }: { data: InitialData }) {
           'flex',
           'px-2 md:px-6',
           'flex-col lg:flex-row',
-          'h-[90vh] w-full gap-4',
+          'h-[87vh] w-full gap-4',
           'overflow-hidden'
         )}
       >
-        {/* <h1>{version}</h1> */}
         <GeneralInfo
           banksData={data}
-          className='h-full w-full  lg:h-[90vh] lg:w-1/3'
+          className='h-full w-full  lg:h-[89vh] lg:w-1/3'
         />
         <Summary
           banksData={data}
-          className='h-full w-full lg:h-[90vh] lg:w-2/3'
+          className='h-full w-full lg:h-[89vh] lg:w-2/3'
         />
       </section>
     </Layout>
   );
 }
 
-/**
- * Used to create delay for page transitions
- * and to render PRIVATE/PUBLIC version of the page.
- */
-function usePageProtection() {
-  const router = useRouter();
-  const [pageIsValidated, setPageIsValidated] = useState(false);
-
-  /**
-   * Using URL parameters to conditionally render Private/Public version of the page.
-   * if URL is missing `/?location=<"demo"|"private">`
-   * use shallow routing to add default `/?location=demo` to the path
-   *
-   * Next JS shallow routing docs: https://nextjs.org/docs/routing/shallow-routing
-   */
-  function validateUrlParams() {
-    const locationParam = router.query['location'];
-    if (locationParam !== 'private') {
-      router.push(`${router.pathname}/?location=demo`, undefined, {
-        shallow: true,
-      });
-    }
-  }
-
-  /**
-   * Check when path generated, to access URL params
-   * https://nextjs.org/docs/api-reference/next/router#router-object
-   */
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-    if (router.isReady) {
-      timer = setTimeout(() => {
-        setPageIsValidated(true);
-        validateUrlParams();
-        // ! popup
-      }, 3000);
-    }
-    return () => clearTimeout(timer);
-  }, [router.isReady]);
-
-  return { pageIsValidated };
+function Loading() {
+  return (
+    <Layout>
+      <div className=' h-full w-full  lg:h-[87vh]'>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-5'>
+          <Image src='/images/logo.png' width={70} height={66} alt='logo' />
+          <Spinner />
+        </div>
+      </div>
+    </Layout>
+  );
 }
