a14a6fadf63d4d553690a6534d88f87f52ba2baa
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-15 18:09:44 -0500

    fix: distance count fixed

--- src/components/shared/Carousel.tsx
+++ src/components/shared/Carousel.tsx
@@ -35,20 +35,20 @@ export function Carousel({
     >
       <button
         className={clsx(
+          'transition-colors duration-100',
+          'lg:invisible lg:group-hover:visible',
           'h-full px-5',
           'absolute top-0 left-0 z-10',
-          'transition-colors duration-100',
-          'pointer-events-none opacity-0',
           'flex-none text-2xl',
-          'bg-gray-600/50',
+          'bg-gray-600/50 lg:bg-gray-600/10',
           'group-hover:bg-gray-400',
-          numberOfChildren > maxNumberOfChildrensInFrame &&
-            index > 0 &&
-            'pointer-events-auto opacity-100'
+          numberOfChildren > maxNumberOfChildrensInFrame && index > 0
+            ? 'pointer-events-auto opacity-100'
+            : 'pointer-events-none opacity-0'
         )}
         onClick={() => setIndex((p) => p - 1)}
       >
-        <HiOutlineChevronLeft />
+        <HiOutlineChevronLeft className='group-hover:text-gray-800' />
       </button>
       <section className={clsx('relative', 'grow ', 'overflow-hidden')}>
         <div
@@ -65,20 +65,20 @@ export function Carousel({
       </section>
       <button
         className={clsx(
+          'transition-colors duration-100',
+          'lg:invisible lg:group-hover:visible',
           'h-full px-5',
           'absolute top-0 right-0',
-          'transition-colors duration-100',
-          'pointer-events-none opacity-0',
           'flex-none text-2xl',
-          'bg-gray-600/50',
+          'bg-gray-600/50 lg:bg-gray-600/10',
           'group-hover:bg-gray-400',
-          numberOfChildren > maxNumberOfChildrensInFrame &&
-            index < numberOfChildren % maxNumberOfChildrensInFrame &&
-            'pointer-events-auto opacity-100'
+          index === numberOfChildren - maxNumberOfChildrensInFrame
+            ? 'pointer-events-none opacity-0'
+            : 'pointer-events-auto opacity-100'
         )}
         onClick={() => setIndex((p) => p + 1)}
       >
-        <HiOutlineChevronRight />
+        <HiOutlineChevronRight className='group-hover:text-gray-800' />
       </button>
     </div>
   );
