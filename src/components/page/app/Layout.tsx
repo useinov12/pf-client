c209656385711733851982c1c1cebc7b4525c7af
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-14 20:37:18 -0500

    style: fade in animation

--- src/components/page/app/Layout.tsx
+++ src/components/page/app/Layout.tsx
@@ -1,15 +1,32 @@
-import { ReactNode } from 'react';
+import clsx from 'clsx';
+import { ReactNode, useEffect, useState } from 'react';
 
+import Sidebar from '@/components/page/app/Sidebar';
 import LayoutCommon from '@/components/shared/LayoutCommon';
 
-import { MobileSidebar, Navbar } from './Navbar';
+import { Navbar } from './Navbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  /* little delay before start page fade-in */
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 100);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <LayoutCommon className='min-h-screen w-screen' withWrapper>
+    <LayoutCommon
+      className={clsx(
+        'h-auto min-h-screen w-screen',
+        isLoaded && 'fade-in-start'
+      )}
+      withWrapper
+    >
       <Navbar />
-      <main className=''>
-        <MobileSidebar />
+      <main>
+        <Sidebar />
         {children}
       </main>
     </LayoutCommon>
