a15d4aeff4a41ea15ebbefae1b1cad12ff3ea967
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-14 20:29:42 -0500

    refactor: SignIn form popup moved to root leveel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
+
 import Layout from '@/components/page/landing/Layout';
-import MainHero from '@/components/page/landing/sections/MainHero';
+import Banner from '@/components/page/landing/sections/Banner';
 import Demo from '@/components/page/landing/sections/Demo';
 import Expirience from '@/components/page/landing/sections/Expirience';
 import Instructions from '@/components/page/landing/sections/Instructions';
+import MainHero from '@/components/page/landing/sections/MainHero';
 import Reviews from '@/components/page/landing/sections/Reviews';
-import Banner from '@/components/page/landing/sections/Banner';
-import { SignInForm } from '@/components/LoginForm/Form';
 
 export default function HomePage() {
   return (
-    <>
-      <Layout>
-        <MainHero />
-        <Demo />
-        <Expirience />
-        <Reviews />
-        <Instructions />
-        <Banner />
-      </Layout>
-      <SignInForm withCloseBtn />
-    </>
+    <Layout>
+      <MainHero />
+      <Demo />
+      <Expirience />
+      <Reviews />
+      <Instructions />
+      <Banner />
+    </Layout>
   );
 }
-
-HomePage.requireAuth = false;
